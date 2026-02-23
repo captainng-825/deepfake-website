203154a549bed05aee4070def0826cdb39d71ed9
captainng-825 <nitishghoshofficial@gmail.com>
2026-02-24 03:16:26 +0530

    fix port binding for render

--- app.py
+++ app.py
@@ -230,9 +230,8 @@ def logout():
 # =========================
 # RUN (Railway Compatible)
 # =========================
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+import os
 
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
